b4a6e0a5bff3150720eb969b8a00fb693a2dc963
2265846l <2265846l@student.gla.ac.uk>
2018-02-27 17:36:10 +0000

    Added template and static file hierarchy, created test index page

--- project_whats_on/project_whats_on/urls.py
+++ project_whats_on/project_whats_on/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from whats_on_dot_com import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", views.index, name="index"),
 ]
--- project_whats_on/whats_on_dot_com/views.py
+++ project_whats_on/whats_on_dot_com/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+# INDEX
+# home page
+def index(request):
+    return HttpResponse("test")
