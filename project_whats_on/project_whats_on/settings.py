7d55bd298c41ad8cc56ed19676a5a5af019926b3
Max Kolle <2247156k@student.gla.ac.uk>
2018-02-28 19:32:09 +0000

    Add html files for main pages, fix urls to be matched in the app instead of the project urls

--- project_whats_on/project_whats_on/settings.py
+++ project_whats_on/project_whats_on/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'project_whats_on.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- project_whats_on/whats_on_dot_com/views.py
+++ project_whats_on/whats_on_dot_com/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 # INDEX
 # home page
 def index(request):
-    return HttpResponse("test")
+    return render(request, 'whats_on_dot_com/index.html', {})
+
+def events(request):
+    return render(request, 'whats_on_dot_com/index.html', {})
+
+def add_event(request):
+    return render(request, 'whats_on_dot_com/index.html', {})
+
+def profiles(request):
+    return render(request, 'whats_on_dot_com/index.html', {})
