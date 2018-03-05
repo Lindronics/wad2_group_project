9be76de7cdeeadf07fd67d9ca2dd544aa15c3661
Max Kolle <2247156k@student.gla.ac.uk>
2018-03-05 00:10:23 +0000

    Add basic html for main pages, add url patterns

--- project_whats_on/whats_on_dot_com/views.py
+++ project_whats_on/whats_on_dot_com/views.py
@@ -4,13 +4,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 # INDEX
 # home page
 def index(request):
-    return render(request, 'whats_on_dot_com/index.html', {})
+    return HttpResponseRedirect('events')  # Via events url pattern
 
 def events(request):
-    return render(request, 'whats_on_dot_com/index.html', {})
+    return render(request, 'whats_on_dot_com/events.html', {})
 
 def add_event(request):
-    return render(request, 'whats_on_dot_com/index.html', {})
+    return render(request, 'whats_on_dot_com/add_event.html', {})
 
 def profiles(request):
-    return render(request, 'whats_on_dot_com/index.html', {})
+    return render(request, 'whats_on_dot_com/profiles.html', {})
