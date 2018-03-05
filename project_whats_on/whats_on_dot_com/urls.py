a1237ad99f85a2938ef1e92ada3d16935e8fe30a
Max Kolle <2247156k@student.gla.ac.uk>
2018-03-05 13:14:57 +0000

    Add templates and whats_on_dot_com urls that for some reason were not tracked after a fast-forward merge

---
+++ project_whats_on/whats_on_dot_com/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+
+from whats_on_dot_com import views
+
+
+urlpatterns = [
+    url(r"^event/add$", views.add_event, name="add_event"),
+    url(r"^profiles$", views.profiles, name="profiles"),
+    url(r"^events$", views.events, name="events"),
+    url(r"^$", views.index, name="index"),  # Provide a redirect to the events page: <hostname>/ maps to <hostname>/events
+]
