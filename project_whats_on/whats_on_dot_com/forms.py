5b0bf4b9693704811563aa29b0cfbc12f413422a
2265846l <2265846l@student.gla.ac.uk>
2018-03-06 15:02:48 +0000

    Started the forms

---
+++ project_whats_on/whats_on_dot_com/forms.py
@@ -0,0 +1,30 @@
+# TODO Some things might be broken for now and loads of stuff is missing. 
+# Will wait for the corresponding pages and views to make some progress
+# and then finish this.
+
+from django import forms
+from whats_on_dot_com.models import UserProfile, Category, Tag, Event
+from django.contrib.auth.models import User
+
+# Used for creating a new event
+class NewEventForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the name of your event.")
+    description = forms.CharField(max_length=1024, help_text="Please enter a description for your event.")
+    date_time = forms.DateTimeField()
+    address = forms.CharField(max_length=128, help_text="Address")
+    location_info = forms.CharField(max_length=128, help_text="Additional location information")
+    event_picture = forms.ImageField()
+    
+    # TODO implement selecting hosts, category, tags when creating event
+    # ideally in a dropdown menu
+
+    class Meta:
+        model = Event
+        exclude = ()
+
+# Used for filtering Events on home page
+class FilterEventsForm(forms.ModelForm):
+    search_term = forms.CharField(max_length=128)
+    
+
+
