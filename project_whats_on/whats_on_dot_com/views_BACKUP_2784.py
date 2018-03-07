93e891a113677d5737569f55ae41d1d85639ded9
Iain Christie <2199288c@student.gla.ac.uk>
2018-03-07 07:55:57 +0000

    merged w/ niklas models

---
+++ project_whats_on/whats_on_dot_com/views_BACKUP_2784.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from whats_on_dot_com.forms import NewEventForm
+
+# INDEX (home page, redirects to events page)
+def index(request):
+    return HttpResponseRedirect('events')  # Via events url pattern
+
+# EVENTS (events page with events in grid list)
+def events(request):
+    return render(request, 'whats_on_dot_com/events.html', {})
+
+# ADD EVENT (for adding a new event)
+@login_required
+def add_event(request):
+    form = NewEventForm()
+
+    # Get data from form, add to model if valid
+    if request.method == 'POST':
+        form = NewEventForm(request.POST)
+
+        if form.is_valid():
+            # TODO further code might be necessary to get tags, hosts, category from form
+            event = form.save(commit=True)
+            print("Event added: %s" % event.name)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'whats_on_dot_com/add_event.html', {"form":form})
+
+# PROFILES (profiles list including search etc.)
+def profiles(request):
+    return render(request, 'whats_on_dot_com/profiles.html', {})
+
+<<<<<<< HEAD
+=======
+# ABOUT (about page with project information)
+def about(request):
+    return render(request, 'whats_on_dot_com/about.html')
+
+>>>>>>> 9d7a7d7b99907a2ade4997da1d1d416db0b65252
+
---
+++ project_whats_on/whats_on_dot_com/views_BASE_952.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+# INDEX
+# home page
+def index(request):
+    return HttpResponseRedirect('events')  # Via events url pattern
+
+def events(request):
+    return render(request, 'whats_on_dot_com/events.html', {})
+
+def add_event(request):
+    return render(request, 'whats_on_dot_com/add_event.html', {})
+
+def profiles(request):
+    return render(request, 'whats_on_dot_com/profiles.html', {})
