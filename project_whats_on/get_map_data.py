7779dfb47dfc7b6d1e5473768f33acf634c8205a
Iain <2199288c@student.gla.ac.uk>
2018-03-15 14:31:15 +0000

    get map data script now returns all valid event objects

--- project_whats_on/get_map_data.py
+++ project_whats_on/get_map_data.py
@@ -3,7 +3,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE","project_whats_on.settings")
 
 
 import django
-import math
+
 
 from django.apps import apps
 from django.conf import settings
@@ -16,7 +16,8 @@ from whats_on_dot_com.models import *
 #https://stackoverflow.com/questions/1916953/filter-zipcodes-by-proximity-in-django-with-the-spherical-law-of-cosines
 '3rd answer down'
 
-def nearby_locations(self, latitude, longitude, radius, max_results=100, use_miles=True):
+
+def nearby_locations(latitude, longitude, radius, max_results=100, use_miles=True):
     if use_miles:
         distance_unit = 3959
     else:
@@ -40,35 +41,11 @@ def nearby_locations(self, latitude, longitude, radius, max_results=100, use_mil
     cursor.execute(sql)
     ids = [row[0] for row in cursor.fetchall()]
 
-    return self.filter(id__in=ids)
-
-nearby_locations(self, 55.8, -4.2, 10, 50)
-#What I want to do in SQL
-#SELECT id, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) )
-#* cos( radians( lng ) - radians(-122) ) + sin( radians(37) )
-#* sin( radians( lat ) ) ) )
-#AS distance FROM markers HAVING distance < 25 ORDER BY distance;
-"""
-def get_queryset():
-    #Using raw sql here as is easier than trying to convert
-    #ideally convert as raw sql is somewhat more vulnerable
-    #see here for details https://docs.djangoproject.com/en/2.0/topics/db/sql/
-    "Change the distance 25 to a variable thats passed in"
-    "Change following hard coded search loc to passed in users loc"
-    search_lat = 55.8   #get from form
-    search_long = -4.2 #get from form
-    desired_dist = 10 #get from form
-    
-    
-    #query = Event.objects.raw('SELECT id, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122) ) + sin( radians(37) ) * sin( radians( lat ) ) ) ) AS distance FROM Event HAVING distance < 25 ORDER BY distance')
-    """
-
-    #for event in Event.objects.raw('SELECT id, ( 3959 * acos( cos( radians(%s) ) * cos( radians( latitude ) ) * cos( radians( %s ) - radians(search_long) ) + sin( radians(37) ) * sin( radians( lat ) ) ) ) AS distance FROM Event HAVING distance < 25 ORDER BY distance', [search_lat, search_long]):
-     #   print (event)
+    return Event.objects.filter(id__in=ids)
+#calling wrong as 55.8 is assigned to self
+print (nearby_locations(55.8, -4.2, 10, 50))
 
 """
-
-
     
     #What PHP does in the example :
     #the PHP code first initializes a new XML document and creates the "markers" parent node. It then connects to the database, executes a SELECT * (select all) query on the markers table, and iterates through the results.
